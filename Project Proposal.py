4608c8019f22a0f8e4fd1714044ef1c941e604e8
Hsuan-Yu Lin <o100249243@gmail.com>
2019-05-23 09:28:24 -0700

    file added

---
+++ Project Proposal.py
@@ -0,0 +1,43 @@
+#Team Cinco Project Proposal
+
+#The kind of data you'd like to work with/field you're interested in (e.g., geodata, weather data, etc.)
+
+#  • Real estate
+#  • Vacation rentals
+#       o	Seattle, WA
+#       o	Boston, MA
+#       o	Austin, TX
+#       o	Chicago, IL
+#       o	San Diego, CA
+
+#   * The kinds of questions you'll be asking of that data
+
+#   • Current market conditions – ability to sell
+#       o	ROI
+#       o	Purchase price per sq ft.
+#       o	Equity change for ownership timeframe 
+#       o	Long term: Days on the market/avg
+#       o	Short term: Number of night available/avg
+
+#   • Vacation rental conditions
+#       o	Saturation of market
+#       o	Short term rentals: Price per night/avg
+#       o	Long term rentals: Price per month/avg
+
+#   • Possible source for such data
+#       o	Inside Airbnb
+#       o	Realtor.com
+#       o	Zillow.com
+
+#       *   Background:
+#               An anonymous benefactor has a $5,000,000 budget and hired a consulting firm to 
+#               come up with recommendations for the best places to purchase real estate.  
+#               The firm must analyze at least five markets in the United States to determine 
+#               if the regions are lucrative investment options.  
+#               Some factors the firm will consider when determining which areas to recommend are: 
+#               ROI, current market conditions, and vacation rental conditions. 
+#
+#       *   Hypothesis:
+#               Rental income in a specific state is determined by type of occupancy short term versus long term. 
+#               Some factors to condider will be vacancy, days on the market and acqusition cost.
+
